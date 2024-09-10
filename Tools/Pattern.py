8e21da561125f852da9dc685b97e685163ece65c
Daehyun Heo <hyeon365@gmail.com>
2024-09-10 13:30:02 +0900

    added chartToMatrix() method

--- Tools/Pattern.py
+++ Tools/Pattern.py
@@ -1,18 +1,41 @@
 import ChartFormat.bms as bms
 import numpy as np
+import pandas as pd
 
 
 class ChartMatrix:
     def __init__(self, chart:bms.BMS):
-        self.chartMatrix = self.chartToMatrix(chart)
+        self.chartMatrix, self.min_dist = self.chartToMatrix(chart)
         
         
-    def chartToMatrix(chart:bms.BMS):
+    def chartToMatrix(self,chart:bms.BMS)-> tuple[pd.DataFrame, float]:
         if not chart.isRead: chart.readAll(); chart.close()
         df = chart.extractToPandas()
-        matrix = df.pivot_table(index='beatstamp', columns='laneInfo', aggfunc='size', fill_value=0)
-        matrix = matrix.reset_index(drop=True)
-        return matrix
+
+        beatstamps = df['beatstamp'].sort_values().unique()
+        min_diff = np.diff(beatstamps).min() # 노트간 최소 비트 거리
+
+        min_beatstamp = beatstamps.min()
+        max_beatstamp = beatstamps.max()
+        beatstamp_points = np.arange(min_beatstamp, max_beatstamp + min_diff, min_diff)
+        # 비트 스탬프 그리드를 위한 Point 생성
+
+        grid = pd.DataFrame(beatstamp_points, columns=['beatstamp'])
+        for lane in df['laneInfo'].unique(): grid[lane] = 0
+        # 포인트로 그리드 생성 및 그리드 완성
+
+        df = df.pivot_table(index='beatstamp', columns='laneInfo', aggfunc='size', fill_value=0).reset_index()
+        # 차트 데이터 프레임을 그리드와 합치기 위한 Form으로 바꿔줌
+        merged_df = pd.merge(grid, df, on='beatstamp', how='left').fillna(0)
+        
+        for col in merged_df.columns:
+            if '_x' in col and col.replace('_x', '_y') in merged_df.columns:
+                merged_df[col.replace('_x', '')] = merged_df[col].fillna(0) + merged_df[col.replace('_x', '_y')].fillna(0)
+                merged_df.drop([col, col.replace('_x', '_y')], axis=1, inplace=True)
+
+        merged_df.set_index('beatstamp', inplace=True)
+        merged_df = merged_df.astype(int)      
+        return (merged_df, min_diff)
         
         
         
--- test.py
+++ test.py
@@ -1,14 +1,13 @@
 import ChartFormat.bms as bms
+from Tools.Pattern import ChartMatrix
+import numpy as np
+import pandas as pd
 
 test = bms.BMS("E:/Backup From Lenovo/BMS Dataset/Filtered Dataset/160 [ANOTHER]_LV10_.bms")
 
 test.readAll()
 df = test.extractToPandas()
 
-matrix = df.pivot_table(index='beatstamp', columns='laneInfo', aggfunc='size', fill_value=0)
+cm = ChartMatrix(test)
 
-# beatstamp 열을 제거하고 행 이름으로만 유지
-matrix_without_beatstamp_column = matrix.reset_index(drop=True)
-
-# 결과 출력
-print(matrix_without_beatstamp_column)
+print(cm.chartMatrix) 
