a924717566742f629f2c9577c3b67634e7dc3236
Daehyun Heo <hyeon365@gmail.com>
2024-09-19 13:40:50 +0900

    added pattern extracting method with flex windows, pattern starts when each note begin

--- Tools/Pattern.py
+++ Tools/Pattern.py
@@ -43,14 +43,10 @@ class ChartMatrix:
             missed_cols = [col for col in LANE_ORDER if col not in merged_df.columns]
             for col in missed_cols:
                 merged_df[col] = 0  # 해당 열을 추가하고 0으로 채움
+        merged_df[merged_df>=2]=1
         return (merged_df, min_diff)
         
         
-        
-def extract_patterns_with_convolution(cm:ChartMatrix,pHeight:int=4, pWidth:int=7, rDim:int=2):
-    matrix = cm.numpy.copy()
-    pass
-    
 
 def extract_patterns_with_flex_window(cm:ChartMatrix,pHeight:int=4, slide:int=1, externalList:list=None)->list[dict]:
     '''패턴 행렬, 패턴의 길이, 그리드 슬라이드를 입력받아 수집된 패턴의 리스트를 반환(외부에서 받을 수도 있음)'''
@@ -92,3 +88,56 @@ def is_equal_pattern_in_list(l:list, target_pattern:np.ndarray)->bool:
             l[i]['appearance'] += 1
             break
     return res
+
+
+def extract_patterns_with_fixed_window(cm:ChartMatrix,pSize:int=3, slide:int=1, externalList:list=None)->list[dict]:
+    '''패턴 행렬, 패턴의 사이즈,(홀수*홀수 인 정방 매트릭스로 제시되어야 함), 그리드 슬라이드를 입력받아 수집된 패턴의 리스트를 반환(외부에서 받을 수도 있음)'''
+    matrix = cm.numpy.copy()
+    rows , cols = matrix.shape
+    if externalList is None : sub_matrice = []
+    else: sub_matrice = externalList
+
+    for i in range(0, rows - pSize +1 ,slide):
+        for j in range(0, cols - pSize +1, slide):
+            c_matrix = matrix[i:i+pSize,j:j+pSize].copy()
+            if not is_equal_pattern_in_list(sub_matrice, c_matrix):
+                sub_matrice.append({'pattern':c_matrix,'appearance':1})
+    sub_matrice = sorted(sub_matrice, key=lambda x:x["appearance"], reverse=True)
+    return sub_matrice
+
+def extract_patterns_from_note_start_w_flex_window(cm:ChartMatrix,pHeight:int=4, externalList:list=None)->list[dict]:
+    matrix = cm.numpy.copy()
+    if externalList is None : sub_matrice = []
+    else: sub_matrice = externalList
+    ps = 0 #pattern start
+    while ps < len(matrix):
+        c_matrix = matrix[ps:ps+pHeight].copy() # 후보 매트릭스 추출
+        left_boader = {"lane": 0, "modified": False}
+        right_boader = {"lane": 7, "modified": False} # 보더 정보 갱신
+
+        for laneNo in range(NUM_OF_LANE): 
+            if left_boader["modified"]== False:
+                for rowNo in range(len(c_matrix)): # 후보 매트릭스의 행수 만큼 확인
+                    if c_matrix[rowNo, laneNo] != 0:
+                        left_boader["lane"] = laneNo
+                        left_boader["modified"] = True
+            if right_boader["modified"]== False:
+                for rowNo in range(len(c_matrix)): # 후보 매트릭스의 행수 만큼 확인
+                    if c_matrix[rowNo, (NUM_OF_LANE-1)-laneNo] != 0:
+                        right_boader["lane"] = (NUM_OF_LANE-1)-laneNo
+                        right_boader["modified"] = True
+            if right_boader["modified"] and left_boader["modified"]: break
+    
+        row_begin = left_boader["lane"];  row_end = right_boader["lane"]
+        c_matrix = c_matrix[:,row_begin:row_end+1]
+
+        if not is_equal_pattern_in_list(sub_matrice, c_matrix):
+            sub_matrice.append({'pattern':c_matrix,'appearance':1})
+
+        while ps < len(matrix):
+            ps += 1
+            if ps >= len(matrix) or not (matrix[ps, :] == 0).all():
+                break
+
+    sub_matrice = sorted(sub_matrice, key=lambda x:x["appearance"], reverse=True)
+    return sub_matrice
